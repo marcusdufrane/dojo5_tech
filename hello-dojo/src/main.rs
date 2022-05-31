8466b961c4871fcd05d0e9ce0882051113b50de8
Marcus DuFrane <marcusdufrane@gmail.com>
2022-05-30 21:28:58 -0500

    Add hal crate, use UART example

--- hello-dojo/src/main.rs
+++ hello-dojo/src/main.rs
@@ -1,20 +1,41 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use panic_halt as _;
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
+use stm32f4xx_hal as hal;
+
+use crate::hal::{pac, prelude::*};
+
+use core::fmt::Write; // for pretty formatting of the serial output
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let dp = pac::Peripherals::take().unwrap();
+
+    let gpioa = dp.GPIOA.split();
+
+    let rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.use_hse(25.MHz()).freeze();
+
+    let mut delay = dp.TIM1.delay_ms(&clocks);
+
+    // define RX/TX pins
+    let tx_pin = gpioa.pa9.into_alternate();
+
+    // configure serial
+    // let mut tx = Serial::tx(dp.USART1, tx_pin, 9600.bps(), &clocks).unwrap();
+    // or
+    let mut tx = dp.USART1.tx(tx_pin, 9600.bps(), &clocks).unwrap();
+
+    let mut value: u8 = 0;
 
     loop {
-        // your code goes here
+        // print some value every 500 ms, value will overflow after 255
+        writeln!(tx, "value: {:02}\r", value).unwrap();
+        value = value.wrapping_add(1);
+        delay.delay(2.secs());
     }
 }
