aff8569db342564989ddec4c395c2d16719fdaa5
Marcus DuFrane <marcusdufrane@gmail.com>
2022-05-30 23:26:55 -0500

    modify memory to use non aliased flash region, say hello

--- hello-dojo/src/main.rs
+++ hello-dojo/src/main.rs
@@ -20,22 +20,14 @@ fn main() -> ! {
 
     let clocks = rcc.cfgr.use_hse(25.MHz()).freeze();
 
-    let mut delay = dp.TIM1.delay_ms(&clocks);
-
     // define RX/TX pins
     let tx_pin = gpioa.pa9.into_alternate();
 
     // configure serial
-    // let mut tx = Serial::tx(dp.USART1, tx_pin, 9600.bps(), &clocks).unwrap();
-    // or
-    let mut tx = dp.USART1.tx(tx_pin, 9600.bps(), &clocks).unwrap();
+    let mut tx = dp.USART1.tx(tx_pin, 115200.bps(), &clocks).unwrap();
 
-    let mut value: u8 = 0;
-
+    writeln!(tx, "Hello Dojo").unwrap();
+    
     loop {
-        // print some value every 500 ms, value will overflow after 255
-        writeln!(tx, "value: {:02}\r", value).unwrap();
-        value = value.wrapping_add(1);
-        delay.delay(2.secs());
     }
 }
